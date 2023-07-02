ea4128adf098036ba70f41e19c71fc7c3f2d5fcc
Daniela <daniela.primacov@yahoo.com>
2023-07-02 13:05:44 +0200

    Working with 'State'

--- 03-React-basics/src/components/Expenses/ExpenseItem.js
+++ 03-React-basics/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpsenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); // React Hook
+
   const clickHandler = () => {
-    console.log("Clicked!");
+    setTitle("Updated!");
   };
 
   return (
     <Card className="expense-item">
       <ExpsenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
