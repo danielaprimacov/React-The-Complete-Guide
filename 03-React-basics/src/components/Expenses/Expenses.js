d5bde8931995e9249af8ad93f0329a7b58436d54
Daniela <daniela.primacov@yahoo.com>
2023-07-07 15:25:28 +0200

    Rendering lists of data

--- 03-React-basics/src/components/Expenses/Expenses.js
+++ 03-React-basics/src/components/Expenses/Expenses.js
@@ -33,7 +33,14 @@ const Expenses = (props) => {
           selected={filterValue}
         />
         {/* <p>Data for years {filterInfoText} is hidden.</p> */}
-        <ExpenseItem
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -52,7 +59,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
