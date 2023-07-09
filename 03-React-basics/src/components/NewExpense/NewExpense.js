ef2d69a0aa464724fd780e2423f156b74edce78a
Daniela <daniela.primacov@yahoo.com>
2023-07-09 07:43:24 +0200

    Add logic to inserting new expenses

--- 03-React-basics/src/components/NewExpense/NewExpense.js
+++ 03-React-basics/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [show, showForm] = useState(false);
+  
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    showForm(false);
   };
+  
+  const showFormHandler = () => {
+    showForm(true);
+  }
+  
+  const closeFormHandler = () => {
+    showForm(false);
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!show && <button onClick={showFormHandler}>Add New Expense</button>}
+      {show && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={closeFormHandler} />}
     </div>
   );
 };
