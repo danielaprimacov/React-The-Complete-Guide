0e7b060ca55bc37eda93d7d26b51f1faa4fe59e4
Daniela <daniela.primacov@yahoo.com>
2023-07-28 14:20:19 +0200

    Managing State & Modal State

--- Food-Order-App/src/components/Layout/Header.js
+++ Food-Order-App/src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>MaxMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Some delicious food" />
