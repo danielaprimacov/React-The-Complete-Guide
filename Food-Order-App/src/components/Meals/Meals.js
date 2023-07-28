291c8bbb6a1c7577efe9e05aef96ca558b694d3b
Daniela <daniela.primacov@yahoo.com>
2023-07-28 13:19:21 +0200

    Adding a 'Meals' Component

---
+++ Food-Order-App/src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AvaibleMeals from "./AvaibleMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+      <AvaibleMeals />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
