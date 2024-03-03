52c3312eb3270c999330e687ae52be6b1dffe88c
Daniela <daniela.primacov@yahoo.com>
2024-03-03 06:45:57 +0100

    Finshing & Using the Cart Context & Reducer

--- Food-Order-App-2/src/components/MealItem.jsx
+++ Food-Order-App-2/src/components/MealItem.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+  
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+  
   return (
-    <li class="meal-item">
+    <li className="meal-item">
       <article>
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
@@ -12,7 +21,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
