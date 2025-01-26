8786d7d01d94aec89123f52010e95e84456c985a
Daniela <daniela.primacov@yahoo.com>
2025-01-26 14:21:23 +0100

    Finishing Touches

--- Food-Order-App-2/src/hooks/useHttp.js
+++ Food-Order-App-2/src/hooks/useHttp.js
@@ -18,21 +18,26 @@ export default function useHttp(url, config, initialData) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
+  function clearData() {
+    setData(initialData);
+  }
+
   const sendRequest = useCallback(
-    async function sendRequest() {
-    setIsLoading(true);
-    try {
-      const resData = await sendHttpRequest(url, config);
-      setData(resData);
-    } catch (error) {
-      setError(error.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-    }, [url, config]
+    async function sendRequest(data) {
+      setIsLoading(true);
+      try {
+        const resData = await sendHttpRequest(url, { ...config, body: data });
+        setData(resData);
+      } catch (error) {
+        setError(error.message || "Something went wrong!");
+      }
+      setIsLoading(false);
+    },
+    [url, config]
   );
-  
+
   useEffect(() => {
-    if ((config && (config.method === 'GET' || !config.method)) || !config) {
+    if ((config && (config.method === "GET" || !config.method)) || !config) {
       sendRequest();
     }
   }, [sendRequest, config]);
@@ -41,6 +46,7 @@ export default function useHttp(url, config, initialData) {
     data,
     isLoading,
     error,
-    sendRequest
+    sendRequest,
+    clearData,
   };
 }
