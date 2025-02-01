604bf6bdf95f50d0fd0c106e8bf322d96ab28cb8
Daniela <daniela.primacov@yahoo.com>
2025-02-01 06:00:45 +0100

    Redux Toolkit, adding state slices

--- 20-redux-project/src/store/index.js
+++ 20-redux-project/src/store/index.js
@@ -1,18 +1,35 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
+// preparing a slice of our global state
+createSlice({
+  name: "counter",
+  initialState,
+  // all the reducers which the state needs
+  reducers: {
+    increment(state) {
+      // mutation is alowed, ReduxToolkit create a clone of the state
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
+    // never mutate (change) the state --> state.counter++
     return {
       counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
       showCounter: state.showCounter,
     };
   }
@@ -20,6 +37,13 @@ const counterReducer = (state = initialState, action) => {
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
       showCounter: state.showCounter,
     };
   }
