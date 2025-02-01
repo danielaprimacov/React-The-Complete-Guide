f639646abbef20ead72b7004e69123f3faca3042
Daniela <daniela.primacov@yahoo.com>
2025-02-01 06:06:09 +0100

    Connecting Redux Toolkit

--- 20-redux-project/src/store/index.js
+++ 20-redux-project/src/store/index.js
@@ -1,10 +1,9 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
 // preparing a slice of our global state
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   // all the reducers which the state needs
@@ -25,39 +24,10 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    // never mutate (change) the state --> state.counter++
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+// manage merging multiple Reducers into one big Reducer
+const store = configureStore({
+  reducer:
+    counterSlice.reducer /*{ counter: counterSlice.reducer }, // a map of reducers */,
+});
 
 export default store;
