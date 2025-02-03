e7a9430521f6923e1e2527fa3f00cf901f48789f
Daniela <daniela.primacov@yahoo.com>
2025-02-03 23:10:37 +0100

    Using useEffect with Redux

--- 21-advanced-redux/src/App.js
+++ 21-advanced-redux/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,15 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    // overwriting the existing cart with the incoming data - PUT method
+    fetch(
+      "https://redux-project-5c357-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
--- 21-advanced-redux/src/store/cart-slice.js
+++ 21-advanced-redux/src/store/cart-slice.js
@@ -29,7 +29,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
-      state.totalAmount--;
+      state.totalQuantity--;
 
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
