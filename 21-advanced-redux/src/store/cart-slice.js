9cf426b52f6c2ace58a8d67dfb41d1366c691594
Daniela <daniela.primacov@yahoo.com>
2025-02-03 23:52:12 +0100

    Using an Action Creator Thunk

--- 21-advanced-redux/src/store/cart-slice.js
+++ 21-advanced-redux/src/store/cart-slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { uiActions } from "./ui-slice";
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
@@ -40,6 +42,52 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// action creator that returns a function which Redux will execute
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    // we can provide any async code
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      // overwriting the existing cart with the incoming data - PUT method
+      const response = await fetch(
+        "https://redux-project-5c357-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sending cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
