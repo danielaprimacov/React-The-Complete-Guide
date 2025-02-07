b736947dbdde11365861fdd38e9e5b81501c250b
Daniela <daniela.primacov@yahoo.com>
2025-02-07 11:57:54 +0100

    Adding a Root Layout

--- 22-project-multipage-spa/frontend/src/App.js
+++ 22-project-multipage-spa/frontend/src/App.js
@@ -8,12 +8,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //    - EventDetailPage
 //    - NewEventPage
 //    - EditEventPage
+
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+
 // 2. Add routing & route definitions for these five pages
 //    - / => HomePage
 //    - /events => EventsPage
 //    - /events/<some-id> => EventDetailPage
 //    - /events/new => NewEventPage
 //    - /events/<some-id>/edit => EditEventPage
+
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
@@ -23,7 +31,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 function App() {
-  return <div></div>;
+  return <RouterProvider />;
 }
 
 export default App;
