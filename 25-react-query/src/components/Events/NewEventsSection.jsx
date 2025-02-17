a94f66070f85fbc9a0baa237744b5d0a19666e32
Daniela <daniela.primacov@yahoo.com>
2025-02-17 21:00:55 +0100

    Using the Query Key as Query Function Input

--- 25-react-query/src/components/Events/NewEventsSection.jsx
+++ 25-react-query/src/components/Events/NewEventsSection.jsx
@@ -8,8 +8,8 @@ import { fetchEvents } from "../../util/http.js";
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     // data contains the response
-    queryKey: ["events"], // can be multiple values and different types
-    queryFn: fetchEvents, // returns a Promise
+    queryKey: ["events", { max: 3 }], // can be multiple values and different types
+    queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }), // returns a Promise
     staleTime: 5000, // controls after which time RQ will send a request
     // gcTime: 1000, // controls how long the data in the Cache will be kept around
   });
