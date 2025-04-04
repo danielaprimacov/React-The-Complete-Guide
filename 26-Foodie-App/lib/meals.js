cf4d2bbf30cbdfe74b664afe1dcf913a0fa173d3
Daniela <daniela.primacov@yahoo.com>
2025-04-04 20:53:11 +0200

    Creating a Slug & Sanitizing User Input

--- 26-Foodie-App/lib/meals.js
+++ 26-Foodie-App/lib/meals.js
@@ -1,13 +1,20 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  
+
   return db.prepare("SELECT * FROM meals").all(); // all - for fetching data
 }
 
 export function getMeal(slug) {
   return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
+
+export function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
 }
