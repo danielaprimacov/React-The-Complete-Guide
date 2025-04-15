bdddf4743c2773db93771abdf4065641a0908bb6
Daniela <daniela.primacov@yahoo.com>
2025-04-15 15:15:17 +0200

    Storing Uploaded Images in the Cloud

--- 26-Foodie-App/lib/meals.js
+++ 26-Foodie-App/lib/meals.js
@@ -1,15 +1,24 @@
-import fs from "node:fs";
+// import fs from 'node:fs';
+import { S3 } from "@aws-sdk/client-s3";
 
 import sql from "better-sqlite3";
 import slugify from "slugify";
 import xss from "xss";
 
+const s3 = new S3({
+  region: "us-east-1",
+  // credentials: {
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // },
+});
 const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  return db.prepare("SELECT * FROM meals").all(); // all - for fetching data
+  // throw new Error('Loading meals failed');
+  return db.prepare("SELECT * FROM meals").all();
 }
 
 export function getMeal(slug) {
@@ -23,30 +32,30 @@ export async function saveMeal(meal) {
   const extension = meal.image.name.split(".").pop();
   const fileName = `${meal.slug}.${extension}`;
 
-  const stream = fs.createWriteStream(`public/images/${fileName}`);
   const bufferedImage = await meal.image.arrayBuffer();
 
-  stream.write(Buffer.from(bufferedImage), (error) => {
-    if (error) {
-      throw new Error("Saving image failed");
-    }
+  s3.putObject({
+    Bucket: "maxschwarzmueller-nextjs-demo-users-image",
+    Key: fileName,
+    Body: Buffer.from(bufferedImage),
+    ContentType: meal.image.type,
   });
 
-  meal.image = `/images/${fileName}`;
+  meal.image = fileName;
 
   db.prepare(
     `
-      INSERT INTO meals 
+    INSERT INTO meals
       (title, summary, instructions, creator, creator_email, image, slug)
-      VALUES (
+    VALUES (
       @title,
       @summary,
       @instructions,
       @creator,
       @creator_email,
       @image,
-      @slug,
-      ) 
-      `
+      @slug
+    )
+  `
   ).run(meal);
 }
