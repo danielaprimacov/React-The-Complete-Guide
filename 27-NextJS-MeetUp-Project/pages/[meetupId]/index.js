7de069f2f5474fdae3fde4907fc64737afbcc3f8
Daniela <daniela.primacov@yahoo.com>
2025-04-24 17:21:15 +0200

    Adding Custom Components

---
+++ 27-NextJS-MeetUp-Project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://cdn.prod.website-files.com/6295a06686070f851ef36b13/62bf4153b49088326e04693e_GettyImages-966266912.jpeg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
