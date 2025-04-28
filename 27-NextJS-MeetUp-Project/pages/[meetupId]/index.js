06e0b5a181634ad718004b2dd7efdcc13df16b54
Daniela <daniela.primacov@yahoo.com>
2025-04-28 17:42:28 +0200

    Working with Params for SSG Data Fetching

--- 27-NextJS-MeetUp-Project/pages/[meetupId]/index.js
+++ 27-NextJS-MeetUp-Project/pages/[meetupId]/index.js
@@ -11,4 +11,25 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://cdn.prod.website-files.com/6295a06686070f851ef36b13/62bf4153b49088326e04693e_GettyImages-966266912.jpeg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
