bba2edf6558a4cd4623099183835898e618ab50a
Daniela <daniela.primacov@yahoo.com>
2025-05-20 19:33:46 +0200

    Working with MongoDB

--- 27-NextJS-MeetUp-Project/pages/api/new-meetup.js
+++ 27-NextJS-MeetUp-Project/pages/api/new-meetup.js
@@ -1,12 +1,22 @@
 // /api/new-meetup
 
+import { MongoClient } from "mongodb";
+
 function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
-    
-    
+    const client = MongoClient.connect();
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
