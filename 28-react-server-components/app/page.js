a975127a2ec23f77b7eb7edc3dcbe3b7cd869aa7
Daniela <daniela.primacov@yahoo.com>
2025-06-11 21:07:29 +0200

    Fetching Data with RSCs

--- 28-react-server-components/app/page.js
+++ 28-react-server-components/app/page.js
@@ -1,12 +1,14 @@
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDemo";
 
 export default function Home() {
   return (
     <main>
-      <ClientDemo>
+      {/* <ClientDemo>
         <RSCDemo />
-      </ClientDemo>
+      </ClientDemo> */}
+      <DataFetchingDemo />
     </main>
   );
 }
