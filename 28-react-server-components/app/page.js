477d839c04aafb3efdd60b879dd39cca3f219cb3
Daniela <daniela.primacov@yahoo.com>
2025-06-13 20:10:51 +0200

    Using the use() Hook for Promises

--- 28-react-server-components/app/page.js
+++ 28-react-server-components/app/page.js
@@ -1,16 +1,23 @@
+import fs from "node:fs/promises";
+
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
+import UsePromiseDemo from "@/components/UsePromiseDemo";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fs.readFile("dummy-db.json", "utf-8");
+  const users = JSON.parse(data);
+
   return (
     <main>
       {/* <ClientDemo>
         <RSCDemo />
       </ClientDemo> */}
       {/* <DataFetchingDemo /> */}
-      <ServerActionsDemo />
+      {/* <ServerActionsDemo /> */}
+      <UsePromiseDemo users={users} />
     </main>
   );
 }
