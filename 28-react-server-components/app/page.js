0168486fa05f4ec07b455ccbb3fb7b7de819348c
Daniela <daniela.primacov@yahoo.com>
2025-06-15 18:46:30 +0200

    The use() Hook in Action

--- 28-react-server-components/app/page.js
+++ 28-react-server-components/app/page.js
@@ -1,3 +1,5 @@
+import fs from "node:fs/promises";
+
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDemo";
@@ -6,6 +8,14 @@ import UsePromiseDemo from "@/components/UsePromiseDemo";
 import { Suspense } from "react";
 
 export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
       {/* <ClientDemo>
@@ -14,7 +24,7 @@ export default async function Home() {
       {/* <DataFetchingDemo /> */}
       {/* <ServerActionsDemo /> */}
       <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo />
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
       </Suspense>
     </main>
   );
--- 28-react-server-components/components/UsePromiseDemo.js
+++ 28-react-server-components/components/UsePromiseDemo.js
@@ -1,13 +1,22 @@
-import fs from "node:fs/promises";
+"use client";
 
-export default async function UsePromiseDemo() {
-  const data = await fs.readFile("dummy-db.json", "utf-8");
-  const users = JSON.parse(data);
+import { useState, use } from "react";
+
+export default function UsePromiseDemo({ usersPromise }) {
+  const users = use(usersPromise);
+  const [count, setCount] = useState(0);
+
   return (
     <div className="rsc">
       <h2>RSC with Data Fetching</h2>
       <p>
         Uses <strong>async / await</strong> for data fetching.
+      </p>
+      <p>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+          Increment
+        </button>
+        <span>{count}</span>
       </p>
       <ul>
         {users.map((user) => (
