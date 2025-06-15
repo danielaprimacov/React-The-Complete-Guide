3446b4725c76f14c8663b2f973a8801ac1a74b0c
Daniela <daniela.primacov@yahoo.com>
2025-06-15 18:40:08 +0200

    use() Hook and Data Fetching

--- 28-react-server-components/app/page.js
+++ 28-react-server-components/app/page.js
@@ -1,15 +1,11 @@
-import fs from "node:fs/promises";
-
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UsePromiseDemo from "@/components/UsePromiseDemo";
+import { Suspense } from "react";
 
 export default async function Home() {
-  const data = await fs.readFile("dummy-db.json", "utf-8");
-  const users = JSON.parse(data);
-
   return (
     <main>
       {/* <ClientDemo>
@@ -17,7 +13,9 @@ export default async function Home() {
       </ClientDemo> */}
       {/* <DataFetchingDemo /> */}
       {/* <ServerActionsDemo /> */}
-      <UsePromiseDemo users={users} />
+      <Suspense fallback={<p>Loading users...</p>}>
+        <UsePromiseDemo />
+      </Suspense>
     </main>
   );
 }
--- 28-react-server-components/components/UsePromiseDemo.js
+++ 28-react-server-components/components/UsePromiseDemo.js
@@ -1,6 +1,10 @@
-export default function UsePromiseDemo({ users }) {
+import fs from "node:fs/promises";
+
+export default async function UsePromiseDemo() {
+  const data = await fs.readFile("dummy-db.json", "utf-8");
+  const users = JSON.parse(data);
   return (
-    <div className='rsc'>
+    <div className="rsc">
       <h2>RSC with Data Fetching</h2>
       <p>
         Uses <strong>async / await</strong> for data fetching.
