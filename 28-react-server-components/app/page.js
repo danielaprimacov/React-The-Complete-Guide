2b8e21bb40e7e71fd10b4eab2c944c2e4b4f5db9
Daniela <daniela.primacov@yahoo.com>
2025-06-15 18:50:27 +0200

    Handling Errors

--- 28-react-server-components/app/page.js
+++ 28-react-server-components/app/page.js
@@ -6,13 +6,15 @@ import RSCDemo from "@/components/RSCDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UsePromiseDemo from "@/components/UsePromiseDemo";
 import { Suspense } from "react";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
 export default async function Home() {
-  const fetchUsersPromise = new Promise((resolve) =>
+  const fetchUsersPromise = new Promise((resolve, reject) =>
     setTimeout(async () => {
       const data = await fs.readFile("dummy-db.json", "utf-8");
       const users = JSON.parse(data);
-      resolve(users);
+      //resolve(users);
+      reject(new Error("Error"));
     }, 2000)
   );
 
@@ -23,9 +25,11 @@ export default async function Home() {
       </ClientDemo> */}
       {/* <DataFetchingDemo /> */}
       {/* <ServerActionsDemo /> */}
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo usersPromise={fetchUsersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
