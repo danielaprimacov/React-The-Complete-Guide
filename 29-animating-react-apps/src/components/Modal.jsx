7428d040c3f805a5775ba5e9a2641d5d3a583190
Daniela <daniela.primacov@yahoo.com>
2025-07-18 15:17:22 +0200

    Animating Element Disappearances / Removal

--- 29-animating-react-apps/src/components/Modal.jsx
+++ 29-animating-react-apps/src/components/Modal.jsx
@@ -8,6 +8,7 @@ export default function Modal({ title, children, onClose }) {
       <motion.dialog
         initial={{ opacity: 0, y: 30 }} // initial state
         animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }} // the state when the element is removed from the DOM
         open
         className="modal"
       >
