3f32753541929b99759f9133769adf6318ccef86
Daniela <daniela.primacov@yahoo.com>
2025-08-01 10:11:11 +0200

    Implementing Search Functionality with Render Props

--- 30-react-patterns/src/App.jsx
+++ 30-react-patterns/src/App.jsx
@@ -1,5 +1,6 @@
 import Accordion from "./components/Accordion/Accordion";
 import SearchableList from "./components/SearchableList/SearchableList";
+import Place from "./Place";
 
 import savannaImg from "./assets/african-savanna.jpg";
 import amazonImg from "./assets/amazon-river.jpg";
@@ -77,8 +78,12 @@ function App() {
         </Accordion>
       </section>
       <section>
-        <SearchableList items={PLACES} />
-        <SearchableList items={["item 1", "item 2"]} />
+        <SearchableList items={PLACES}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList items={["item 1", "item 2"]}>
+          {(item) => item}
+        </SearchableList>
       </section>
     </main>
   );
