14284235b98dc36852183e648abc11d7dc18a56b
Daniela <daniela.primacov@yahoo.com>
2025-08-01 09:52:40 +0200

    Sharing Cross-Component State

--- 30-react-patterns/src/App.jsx
+++ 30-react-patterns/src/App.jsx
@@ -7,13 +7,10 @@ function App() {
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
           <Accordion.Item id="experience" className="accordion-item">
-            <Accordion.Title id="experience" className="accordion-item-title">
+            <Accordion.Title className="accordion-item-title">
               We got 20 years of experience
             </Accordion.Title>
-            <Accordion.Content
-              id="experience"
-              className="accordion-item-content"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>You can&apos;t wrong with us.</p>
                 <p>
@@ -24,13 +21,10 @@ function App() {
             </Accordion.Content>
           </Accordion.Item>
           <Accordion.Item id="local-guides" className="accordion-item">
-            <Accordion.Title id="local-guides" className="accordion-item-title">
+            <Accordion.Title className="accordion-item-title">
               We're working with local guides
             </Accordion.Title>
-            <Accordion.Content
-              id="local-guides"
-              className="accordion-item-content"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>We are not doing this along from our office.</p>
                 <p>
