e911368b11f823f819ff6979437f4fd30b062f3a
Daniela <daniela.primacov@yahoo.com>
2025-08-07 20:17:32 +0200

    Writing the First Test

---
+++ 32-testing-react-apps/src/components/Greeting.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("renders Hellor World as a text", () => {
+  // 3A
+  // 1 - Arrange - set up the test data, test conditions and test environment
+  render(<Greeting />);
+
+  // 2 - Act - run logic that should be tested
+  // ... nothing
+
+  // 3 - Assert - compare execution results with expected results
+  const helloWorldElement = screen.getByText("Hello World", { exact: false });
+  expect(helloWorldElement).toBeInTheDocument();
+});
