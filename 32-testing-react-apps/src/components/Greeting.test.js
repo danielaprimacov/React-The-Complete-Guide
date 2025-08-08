c600d2de41d6d703f02de549466b12ac95310e12
Daniela <daniela.primacov@yahoo.com>
2025-08-08 19:29:54 +0200

    Grouping tests together with Test Suites

--- 32-testing-react-apps/src/components/Greeting.test.js
+++ 32-testing-react-apps/src/components/Greeting.test.js
@@ -1,15 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hellor World as a text", () => {
-  // 3A
-  // 1 - Arrange - set up the test data, test conditions and test environment
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hellor World as a text", () => {
+    // 3A
+    // 1 - Arrange - set up the test data, test conditions and test environment
+    render(<Greeting />);
 
-  // 2 - Act - run logic that should be tested
-  // ... nothing
+    // 2 - Act - run logic that should be tested
+    // ... nothing
 
-  // 3 - Assert - compare execution results with expected results
-  const helloWorldElement = screen.getByText("Hello World", { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+    // 3 - Assert - compare execution results with expected results
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
