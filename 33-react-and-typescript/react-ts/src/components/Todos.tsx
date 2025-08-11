b65f5c732cf56f1b518d83c32042a94a6a423076
Daniela <daniela.primacov@yahoo.com>
2025-08-11 17:47:24 +0200

    Adding Styling

--- 33-react-and-typescript/react-ts/src/components/Todos.tsx
+++ 33-react-and-typescript/react-ts/src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import Todo from "../models/todos";
 import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
 
 // React.FC is a type definition - make clear that this is a function (functional component)
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem key={item.id} text={item.text} />
       ))}
