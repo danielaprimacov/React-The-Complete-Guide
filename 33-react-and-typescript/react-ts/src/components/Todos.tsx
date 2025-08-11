dc2be8371302074e21c04135fcf96ef05fa4c172
Daniela <daniela.primacov@yahoo.com>
2025-08-11 17:11:35 +0200

    Adding the list item component

--- 33-react-and-typescript/react-ts/src/components/Todos.tsx
+++ 33-react-and-typescript/react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import Todo from "../models/todos";
+import TodoItem from "./TodoItem";
 
 // React.FC is a type definition - make clear that this is a function (functional component)
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
