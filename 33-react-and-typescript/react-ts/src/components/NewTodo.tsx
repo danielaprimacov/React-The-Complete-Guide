39f5f272dd03176792902618cbafc1f5fe697b37
Daniela <daniela.primacov@yahoo.com>
2025-08-11 17:27:57 +0200

    Working with refs & useRef

--- 33-react-and-typescript/react-ts/src/components/NewTodo.tsx
+++ 33-react-and-typescript/react-ts/src/components/NewTodo.tsx
@@ -1,6 +1,17 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
     
     
   };
@@ -8,7 +19,7 @@ const NewTodo = () => {
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="todo">Todo text</label>
-      <input type="text" id="todo" />
+      <input type="text" id="todo" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
