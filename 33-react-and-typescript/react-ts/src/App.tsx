b3ddaec2e2ff06e6356c5d91b8d8553f9e811d97
Daniela <daniela.primacov@yahoo.com>
2025-08-11 17:56:36 +0200

    Removing a Todo

--- 33-react-and-typescript/react-ts/src/App.tsx
+++ 33-react-and-typescript/react-ts/src/App.tsx
@@ -15,10 +15,16 @@ function App() {
     });
   };
 
+  const deleteTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} deleteTodo={deleteTodoHandler} />
     </div>
   );
 }
--- 33-react-and-typescript/react-ts/src/components/Todos.tsx
+++ 33-react-and-typescript/react-ts/src/components/Todos.tsx
@@ -3,11 +3,17 @@ import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
 // React.FC is a type definition - make clear that this is a function (functional component)
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; deleteTodo: (id: string) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          deleteTodo={props.deleteTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
