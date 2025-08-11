9e9cd6f3ebfc36284dca55016b578cbee2db1bdd
Daniela <daniela.primacov@yahoo.com>
2025-08-11 16:55:09 +0200

    Working with Props & TypeScript

--- 33-react-and-typescript/react-ts/src/components/Todos.tsx
+++ 33-react-and-typescript/react-ts/src/components/Todos.tsx
@@ -1,10 +1,12 @@
-function Todos() {
+// React.FC is a type definition - make clear that this is a function (functional component)
+const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
